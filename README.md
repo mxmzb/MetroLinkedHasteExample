e3b0acdeb5de7c284e84819196cf95529351992c
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2018-10-08 12:48:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# to see the error
+
+1. cd into this directory
+3. `cd RNapp && yarn && cd ..`
+2. `ln -s scoped-module RNapp/node_modules/@scope/ui`
+4. `cd RNapp && yarn start`
---
+++ RNapp/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+import test from '@scope/ui'
+
+AppRegistry.registerComponent(appName, () => App);
